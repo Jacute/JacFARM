19e386756ab2a5eeeba246dd184fc3b01a3ec820
Jacute <Eri228@yandex.ru>
2025-07-31 10:21:27 +0700

    refactor: compose models, dto

--- backend/internal/models/flag.go
+++ backend/internal/models/flag.go
@@ -2,8 +2,18 @@ package models
 
 type FlagStatus string
 
+const (
+	FlagStatusPending    FlagStatus = "pending"
+	FlagStatusInProgress FlagStatus = "in_progress"
+	FlagStatusCompleted  FlagStatus = "completed"
+	FlagStatusFailed     FlagStatus = "failed"
+)
+
 type Flag struct {
-	Value       string `json:"value"`
-	ExploitName string `json:"exploit_name"`
-	GetFrom     *Team  `json:"victim_team"`
+	ID                string     `json:"id"`
+	Value             string     `json:"value"`
+	Status            FlagStatus `json:"status"`
+	Exploit           *Exploit   `json:"exploit"`
+	GetFrom           *Team      `json:"victim_team"`
+	MessageFromServer string     `json:"message_from_server"`
 }
