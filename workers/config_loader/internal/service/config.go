f55b82aabd8d635378fb22b08c12ddae9c224baf
Jacute <Eri228@yandex.ru>
2025-08-09 14:44:10 +0700

    refactor: add common constants in pkg

--- workers/config_loader/internal/service/config.go
+++ workers/config_loader/internal/service/config.go
@@ -4,26 +4,13 @@ import (
 	"config_loader/internal/config"
 	"config_loader/internal/models"
 	"config_loader/internal/postgres"
+	"config_loader/pkg/common_config"
 	"context"
 	"errors"
 	"log/slog"
 	"strconv"
 
 	"github.com/jacute/prettylogger"
-)
-
-const (
-	ConfigFlagFormatKey           = "EXPLOIT_RUNNER_FLAG_FORMAT"
-	ConfigExploitDuration         = "EXPLOIT_RUNNER_DURATION"
-	ConfigExploitMaxWorkingTime   = "EXPLOIT_RUNNER_MAX_WORKING_TIME"
-	ConfigMaxConcurrentExploits   = "EXPLOIT_RUNNER_MAX_CONCURRENT_EXPLOITS"
-	ConfigFlagSenderPlugin        = "FLAG_SENDER_PLUGIN"
-	ConfigFlagSenderSubmitTimeout = "FLAG_SENDER_SUBMIT_TIMEOUT"
-	ConfigFlagSenderSubmitPeriod  = "FLAG_SENDER_SUBMIT_PERIOD"
-	ConfigFlagSenderJuryFlagURL   = "FLAG_SENDER_JURY_FLAG_URL_OR_HOST"
-	ConfigFlagSenderToken         = "FLAG_SENDER_TOKEN"
-	ConfigFlagSenderSubmitLimit   = "FLAG_SENDER_SUBMIT_LIMIT"
-	ConfigFlagSenderFlagTTL       = "FLAG_SENDER_FLAG_TTL"
 )
 
 func (s *Service) LoadConfigIntoDB(ctx context.Context, cfg *config.Config) error {
@@ -50,18 +37,18 @@ func (s *Service) LoadConfigIntoDB(ctx context.Context, cfg *config.Config) erro
 
 	var existParams []string
 	configMap := map[string]string{
-		ConfigFlagFormatKey:         cfg.ExploitRunner.FlagFormat,
-		ConfigExploitDuration:       cfg.ExploitRunner.RunDuration.String(),
-		ConfigExploitMaxWorkingTime: cfg.ExploitRunner.ExploitMaxWorkingTime.String(),
-		ConfigMaxConcurrentExploits: strconv.Itoa(cfg.ExploitRunner.MaxConcurrentExploits),
+		common_config.ConfigFlagFormatKey:         cfg.ExploitRunner.FlagFormat,
+		common_config.ConfigExploitDuration:       cfg.ExploitRunner.RunDuration.String(),
+		common_config.ConfigExploitMaxWorkingTime: cfg.ExploitRunner.ExploitMaxWorkingTime.String(),
+		common_config.ConfigMaxConcurrentExploits: strconv.Itoa(cfg.ExploitRunner.MaxConcurrentExploits),
 
-		ConfigFlagSenderFlagTTL:       cfg.FlagSender.FlagTTL.String(),
-		ConfigFlagSenderJuryFlagURL:   cfg.FlagSender.JuryFlagURL,
-		ConfigFlagSenderPlugin:        cfg.FlagSender.Plugin,
-		ConfigFlagSenderSubmitTimeout: cfg.FlagSender.SubmitTimeout.String(),
-		ConfigFlagSenderSubmitLimit:   strconv.Itoa(cfg.FlagSender.SubmitLimit),
-		ConfigFlagSenderSubmitPeriod:  cfg.FlagSender.SubmitPeriod.String(),
-		ConfigFlagSenderToken:         cfg.FlagSender.Token,
+		common_config.ConfigFlagSenderFlagTTL:       cfg.FlagSender.FlagTTL.String(),
+		common_config.ConfigFlagSenderJuryFlagURL:   cfg.FlagSender.JuryFlagURL,
+		common_config.ConfigFlagSenderPlugin:        cfg.FlagSender.Plugin,
+		common_config.ConfigFlagSenderSubmitTimeout: cfg.FlagSender.SubmitTimeout.String(),
+		common_config.ConfigFlagSenderSubmitLimit:   strconv.Itoa(cfg.FlagSender.SubmitLimit),
+		common_config.ConfigFlagSenderSubmitPeriod:  cfg.FlagSender.SubmitPeriod.String(),
+		common_config.ConfigFlagSenderToken:         cfg.FlagSender.Token,
 	}
 
 	for k, v := range configMap {
